6251a2a9e7e602cef9b059ac90f89604cad3626c
Thorbjørn Djupvik <thorbjorn.djupvik@gmail.com>
2025-06-24 14:39:11 +0200

    removed build and install from tracking

--- install/include/lib_a.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <string>
-
-namespace lib_a {
-    std::string get_message_a();
-} // namespace lib_a
--- install/include/lib_a2.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-namespace lib_a {
-    int add_numbers(int a, int b);
-} // namespace lib_a
-
