8393cc7b18ab7997b1be222e12d10f710a283780
Thorbjørn Djupvik <thorbjorn.djupvik@gmail.com>
2025-06-25 09:15:43 +0200

    updated flake.nix

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
           src = self;
           version = "git";
           buildInputs = with pkgs; [ cmake gcc ];
-          configurePhase = "cmake -S $src -B build -DCMAKE_INSTALL_PREFIX=$out";
+          configurePhase = "echo $PWD && ls && echo ${src} && ls ${src} && cmake -S $src -B build -DCMAKE_INSTALL_PREFIX=$out";
           buildPhase = "cmake --build build";
           # installPhase = "mkdir -p $out/bin; install -t $out/bin hello";
           installPhase = "cmake --install build";
